4a9fefab0bb05a2efccbf5a6f095837b132c4a66
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-01 20:33:42 -0700

    Add Tamperer

---
+++ tamperer.lua
@@ -0,0 +1,12 @@
+local package = {}
+package.name = "tamperer"
+package.instdat = {}
+package.instdat.repo_owner = "Fatboychummy-CC"
+package.instdat.repo_name = "Tamperer"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["minified.lua"] = "/lib/tamperer.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
