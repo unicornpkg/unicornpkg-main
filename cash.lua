c53bc46476aa3bc5ad987b86ff5facd03023dbba
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-02 20:34:46 -0700

    Add cash

---
+++ cash.lua
@@ -0,0 +1,13 @@
+local package = {}
+package.name = "cash"
+package.instdat = {}
+package.instdat.repo_owner = "MCJack123"
+package.instdat.repo_name = "cash"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["cash.lua"] = "/bin/cash.lua"
+package.instdat.filemaps["env.lua"] = "/bin/env.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
