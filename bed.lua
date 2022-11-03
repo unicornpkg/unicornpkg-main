fbdcdffc3c94399d159428a28c95037bfe64a347
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-02 19:49:39 -0700

    Add bed binary editor

---
+++ bed.lua
@@ -0,0 +1,12 @@
+local package = {}
+package.name = "bed"
+package.instdat = {}
+package.instdat.repo_owner = "simadude"
+package.instdat.repo_name = "bedcc"
+package.instdat.repo_ref = "main"
+package.instdat.filemaps = {}
+package.instdat.filemaps["bed.lua"] = "/bin/bed.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
