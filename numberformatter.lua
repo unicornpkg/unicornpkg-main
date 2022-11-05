693a1d137472f784073630dcbb56d1283285e619
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-04 19:27:36 -0700

    Add numberformatter

---
+++ numberformatter.lua
@@ -0,0 +1,12 @@
+local package = {}
+package.name = "numberformatter"
+package.instdat = {}
+package.instdat.repo_owner = "Commandcracker"
+package.instdat.repo_name = "youcube"
+package.instdat.repo_ref = "main"
+package.instdat.filemaps = {}
+package.instdat.filemaps["client/lib/numberformatter.lua"] = "/lib/numberformatter.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
