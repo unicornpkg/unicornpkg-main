fcd737db754fc3d4dc7a9e56288f51001c7da321
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-15 19:56:17 -0800

    Add ink

---
+++ ink.lua
@@ -0,0 +1,10 @@
+local package = {}
+package.name = "ink"
+package.desc = "Graphical word processor"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["JxzXPv03"] = "/bin/ink.lua"
+package.pkgType = "com.pastebin"
+package.unicornSpec = "v1.0.0"
+
+return package
