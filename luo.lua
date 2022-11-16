7ea850a825fec5a76efee49e21f44558ae4e241d
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-15 20:10:35 -0800

    Add Luo

---
+++ luo.lua
@@ -0,0 +1,10 @@
+local package = {}
+package.name = "luo"
+package.desc = "Advanced Lua-based programming language"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["WwTKkiiU"] = "/bin/luo.lua"
+package.pkgType = "com.pastebin"
+package.unicornSpec = "v1.0.0"
+
+return package
