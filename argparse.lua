c087b6659991bd9c9ec23899ca242e628e01703b
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-03 20:43:05 -0800

    Add argparse

---
+++ argparse.lua
@@ -0,0 +1,14 @@
+local package = {}
+package.name = "argparse"
+package.desc = "Command-line argument parsing library for ComptuerCraft"
+package.version = "0.6.0"
+package.instdat = {}
+package.instdat.repo_owner = "mpeterv"
+package.instdat.repo_name = "argparse"
+package.instdat.repo_ref = "0.6.0" -- This project doesn't have a "v" prefix on their tags
+package.instdat.filemaps = {}
+package.instdat.filemaps["src/argparse.lua"] = "/lib/argparse.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
