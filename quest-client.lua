3d3e41d93bcf832d9f0184bdc7adf178bb46ee95
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-04 17:19:57 -0800

    Add Quest browser

---
+++ quest-client.lua
@@ -0,0 +1,10 @@
+local package = {}
+package.name = "quest-client"
+package.desc = "HTML-based web browser for ComputerCraft"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["VDUGPdiA"] = "/bin/quest.lua"
+package.pkgType = "com.pastebin"
+package.unicornSpec = "v1.0.0"
+
+return package
---
+++ quest-server.lua
@@ -0,0 +1,10 @@
+local package = {}
+package.name = "quest-server"
+package.desc = "Server for serving CCML pages to a Quest client"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["qpWzu0HA"] = "/bin/quest-server.lua"
+package.pkgType = "com.pastebin"
+package.unicornSpec = "v1.0.0"
+
+return package
