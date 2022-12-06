82e4abae05fb55d7c5c3c991364695ec80f82578
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2022-12-06 21:14:56 +0100

    Add luaide (#8)

---
+++ luaide.lua
@@ -0,0 +1,16 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://github.com/benanders/LuaIDE
+
+local package = {}
+package.name = "luaide"
+package.desc = "An in-game IDE for ComputerCraft"
+package.instdat = {}
+package.instdat.repo_owner = "benanders"
+package.instdat.repo_name = "LuaIDE"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["luaide.lua"] = "/bin/luaide.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
