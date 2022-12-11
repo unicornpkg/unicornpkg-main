77eb09c839192b2bfe0f5940bcb10277c49e1a03
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-10 20:05:34 -0800

    Add giim

---
+++ giim.lua
@@ -0,0 +1,13 @@
+local package = {}
+package.name = "giim"
+package.desc = "Modular image editor for ComputerCraft"
+package.instdat = {}
+package.instdat.repo_owner = "MasonGulu"
+package.instdat.repo_name = "cc-giim"
+package.instdat.repo_ref = "master" -- This project does not have versions
+package.instdat.filemaps = {}
+package.instdat.filemaps["giim.lua"] = "/bin/giim.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
