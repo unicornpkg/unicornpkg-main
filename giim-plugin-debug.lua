9ded56295f66f1ff4a5c196ab4890199d9836538
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-10 20:46:00 -0800

    Add giim plugins

--- giim-plugin-debug.lua
+++ giim-plugin-debug.lua
@@ -1,14 +1,13 @@
-fs.makeDir("/lib/giim")
-
 local package = {}
-package.name = "giim"
-package.desc = "Modular image editor for ComputerCraft"
+package.name = "giim-plugin-nfp"
+package.desc = "giim image editor extension that prints all loaded plugins"
 package.instdat = {}
 package.instdat.repo_owner = "MasonGulu"
 package.instdat.repo_name = "cc-giim"
 package.instdat.repo_ref = "master" -- This project does not have versions
 package.instdat.filemaps = {}
-package.instdat.filemaps["giim.lua"] = "/bin/giim.lua"
+package.instdat.filemaps["gplugins/debug.lua"] = "/lib/giim/debug.lua"
+package.rel.depends = {"giim"}
 package.pkgType = "com.github"
 package.unicornSpec = "v1.0.0"
 
--- giim-plugin-nfp.lua
+++ giim-plugin-nfp.lua
@@ -1,14 +1,13 @@
-fs.makeDir("/lib/giim")
-
 local package = {}
-package.name = "giim"
-package.desc = "Modular image editor for ComputerCraft"
+package.name = "giim-plugin-nfp"
+package.desc = "NFP file support for the giim image editor"
 package.instdat = {}
 package.instdat.repo_owner = "MasonGulu"
 package.instdat.repo_name = "cc-giim"
 package.instdat.repo_ref = "master" -- This project does not have versions
 package.instdat.filemaps = {}
-package.instdat.filemaps["giim.lua"] = "/bin/giim.lua"
+package.instdat.filemaps["gplugins/nfp.lua"] = "/lib/giim/nfp.lua"
+package.rel.depends = {"giim"}
 package.pkgType = "com.github"
 package.unicornSpec = "v1.0.0"
 
