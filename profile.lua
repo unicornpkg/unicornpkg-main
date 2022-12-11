3fb83e6bcccb6072ced73fa421dbe418ec51fe71
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-10 19:12:42 -0800

    Add profiler

---
+++ profile.lua
@@ -0,0 +1,13 @@
+local package = {}
+package.name = "profile"
+package.desc = "Simple program that times how long a script takes to execute"
+package.instdat = {}
+package.instdat.repo_owner = "MasonGulu"
+package.instdat.repo_name = "a995c4eaad02cb3bb02fa1e91d711d46"
+package.instdat.repo_ref = "2da6dd946f57f28d70f39b10b3cf311cdbcda4aca01ba6cb6e85175804fd096a"
+package.instdat.filemaps = {}
+package.instdat.filemaps["profile.lua"] = "/bin/profile.lua"
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
+
+return package
