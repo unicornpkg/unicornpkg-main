c752a353f4f26e0cac6aed40ef49ac04b4be35c4
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-10 21:51:12 -0800

    Add some Mekanism scripts

---
+++ mekanism-fission-reactor-control.lua
@@ -0,0 +1,15 @@
+-- https://gitlab.com/peekofwar-craftos-programs/mekanism-fission-reactor-control
+
+local package = {}
+package.name = "mekanism-fission-reactor-control"
+package.desc = "Control a Mekanism fission reactor automatically"
+package.instdat = {}
+package.instdat.repo_owner = "peekofwar-craftos-programs"
+package.instdat.repo_name = "mekanism-fission-reactor-control"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["reactor_control.lua"] = "/bin/mekanism_fission_reactor_control.lua"
+package.pkgType = "com.gitlab"
+package.unicornSpec = "v1.0.0"
+
+return package
---
+++ mekanism-turbine-control.lua
@@ -0,0 +1,15 @@
+-- https://gitlab.com/peekofwar-craftos-programs/mekanism-fission-reactor-control
+
+local package = {}
+package.name = "mekanism-turbine-control"
+package.desc = "Control your Mekanism turbine automatically"
+package.instdat = {}
+package.instdat.repo_owner = "peekofwar-craftos-programs"
+package.instdat.repo_name = "mekanism-fission-reactor-control"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["turbine_control.lua"] = "/bin/mekanism_turbine_control.lua"
+package.pkgType = "com.gitlab"
+package.unicornSpec = "v1.0.0"
+
+return package
