66fd4b357c1fd935664a70760529a62612648433
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-10 20:57:30 -0800

    Fix giim plugin packages

--- giim-plugin-debug.lua
+++ giim-plugin-debug.lua
@@ -7,6 +7,7 @@ package.instdat.repo_name = "cc-giim"
 package.instdat.repo_ref = "master" -- This project does not have versions
 package.instdat.filemaps = {}
 package.instdat.filemaps["gplugins/debug.lua"] = "/lib/giim/debug.lua"
+package.rel = {}
 package.rel.depends = {"giim"}
 package.pkgType = "com.github"
 package.unicornSpec = "v1.0.0"
--- giim-plugin-nfp.lua
+++ giim-plugin-nfp.lua
@@ -7,6 +7,7 @@ package.instdat.repo_name = "cc-giim"
 package.instdat.repo_ref = "master" -- This project does not have versions
 package.instdat.filemaps = {}
 package.instdat.filemaps["gplugins/nfp.lua"] = "/lib/giim/nfp.lua"
+package.rel = {}
 package.rel.depends = {"giim"}
 package.pkgType = "com.github"
 package.unicornSpec = "v1.0.0"
