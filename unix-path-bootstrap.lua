173421cc7c219bbe4805abe1e6d142ce089911e4
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-18 11:01:29 -0800

    unix-path-bootstrap: Use dirs module

--- unix-path-bootstrap.lua
+++ unix-path-bootstrap.lua
@@ -1,10 +1,3 @@
-local fs = fs
-if _HOST:find("Recrafted") then
-	fs = require("fs")
-end
-
-fs.makeDir("/startup")
-
 local package = {}
 package.name = "unix-path-bootstrap"
 package.desc = "Adds /bin to the shell's path and /usr/share/help to the help path."
@@ -15,6 +8,7 @@ package.instdat.repo_ref = "0dbe8a7ef58ac6f6a562410fafe7654301ffc678"
 package.instdat.filemaps = {}
 package.instdat.filemaps["unix-path-bootstrap.lua"] = "/bin/unix-path-bootstrap.lua"
 package.instdat.filemaps["unix-path-bootstrap-startup.lua"] = "/startup/unix-path-bootstrap.lua"
+package.dirs = {"/startup"}
 package.pkgType = "com.github.gist"
 package.unicornSpec = "v1.0.0"
 
