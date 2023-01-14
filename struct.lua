ebe9342aa2fe3758890f82ad0d47097e35e55045
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-01-13 19:37:49 -0800

    Add struct.lua

---
+++ struct.lua
@@ -0,0 +1,16 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://gist.github.com/MCJack123/2e60f0b1c01411f4fe91d902212e33c9
+
+local package = {}
+package.name = "struct.lua"
+package.desc = "Lua 5.3+ C struct parser library for declarative binary file serialization"
+package.instdat = {}
+package.instdat.repo_owner = "MCJack123"
+package.instdat.repo_name = "2e60f0b1c01411f4fe91d902212e33c9"
+package.instdat.repo_ref = "9706c7b8966abd101227ce8a3399b911ea4aee29"
+package.instdat.filemaps = {}
+package.instdat.filemaps["struct.lua"] = "/lib/struct.lua"
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
+
+return package
