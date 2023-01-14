35a87ffe850d512b7073ac4992073b7eb5866cd3
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-01-13 19:23:08 -0800

    Add TypeScript bundle

---
+++ typescript.lua
@@ -0,0 +1,13 @@
+local package = {}
+package.name = "typescript"
+package.desc = "Support for TypeScriptToLua on ComputerCraft systems"
+package.instdat = {}
+package.instdat.repo_owner = "tomodachi94"
+package.instdat.repo_name = "10818bc9dc3ab19364ff0544ad1709c7"
+package.instdat.repo_ref = "2c5aa7de50e4cbc71ddd7145563a5b63e88c97dd46a8991e64f0919666f01c98"
+package.instdat.filemaps = {}
+package.instdat.filemaps["lualib_bundle.lua"] = "/lib/lualib_bundle.lua"
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
+
+return package
