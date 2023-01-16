269e0a7ceb779fc7b247f6c4e0d379c57cd7997b
Marcus Wenzel <znepb@outlook.com>
2023-01-16 02:50:59 -0500

    Add some Snowflake-Software and znepb packages (#10)

---
+++ cropfarm.lua
@@ -0,0 +1,17 @@
+local package = {}
+package.name = "cropfarm"
+package.desc = "A simple and destructive farmer for ComputerCraft turtles."
+package.instdat = {}
+package.instdat.repo_owner = "znepb"
+package.instdat.repo_name = "b373e1d3806ce747a059fb8ca5379924"
+package.instdat.repo_ref = "3f6e3736d042b9211b8ebf854eebcf25394d74ae"
+package.instdat.filemaps = {}
+package.instdat.filemaps["cropfarm.lua"] = "/bin/cropfarm.lua"
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
+package.rel = {}
+package.rel.depends = {
+	"tuttle"
+}
+
+return package
