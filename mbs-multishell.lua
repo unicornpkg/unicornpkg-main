7edea5157d1739d64522cabe986a9b2c677a5060
Tomo <68489118+tomodachi94@users.noreply.github.com>
2023-01-18 11:07:52 -0800

    mbs-multishell: Init

---
+++ mbs-multishell.lua
@@ -0,0 +1,13 @@
+local package = {}
+package.name = "mbs-multishell"
+package.desc = "Tabbing extension for Mildly Better Shell"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["pQmFngWrMc"] = "/.mbs/modules/multishell.lua"
+package.instdat.filemaps["72YJ2u6Kmp"] = "/.mbs/bin/multishell.lua"
+package.rel = {}
+package.rel.depends = { "mbs" }
+package.pkgType = "io.sc3.p"
+package.unicornSpec = "v1.0.0"
+
+return package
