d0cd164a9ae262f9b6692ac6d92aadc87cb3394d
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-02-09 18:02:17 -0800

    billboard: init at 2022-09-01

---
+++ billboard.lua
@@ -0,0 +1,14 @@
+local package = {}
+package.name = "billboard"
+package.desc = "Simple program to display a BIMG file onto a `term`-capable peripheral"
+package.version = "2022-09-01" -- No versioning scheme is present, so we use an ISO date
+package.instdat = {}
+package.instdat.repo_owner = "MasonGulu"
+package.instdat.repo_name = "27d3ea40830fff6eeab5d47ea5a6a048"
+package.instdat.repo_ref = "992c70baf86f0516466af19987cfd6e7bf0e0359"
+package.instdat.filemaps = {}
+package.instdat.filemaps["billboard.lua"] = "/bin/billboard.lua"
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
+
+return package
