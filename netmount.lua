c3d6609a660849571f5aff1094279a1c07e3173d
Tomo <68489118+tomodachi94@users.noreply.github.com>
2023-08-27 18:04:53 -0700

    netmount: init

---
+++ netmount.lua
@@ -0,0 +1,14 @@
+local package = {}
+package.name = "netmount"
+package.version = "202308.27.1"
+package.desc = "Websocket-based file transfer system."
+package.instdat = {}
+package.instdat.repo_owner = "tmpim"
+package.instdat.repo_name = "netmountcc"
+package.instdat.repo_ref = "2227b5623ff21a928beeab97b65269691bd20a1a"
+package.instdat.filemaps = {}
+package.instdat.filemaps["public/mount.lua"] = "/bin/netmount.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
