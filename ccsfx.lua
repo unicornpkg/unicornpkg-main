74c20b361b2e3b40712efec332d763168321837a
Tomo <68489118+tomodachi94@users.noreply.github.com>
2023-09-01 21:31:49 -0700

    Create ccsfx.lua at 1.1.0

---
+++ ccsfx.lua
@@ -0,0 +1,11 @@
+local package = {}
+package.name = "ccsfx"
+package.desc = "Self-extracting archive creator"
+package.version = "1.1.0"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["rxnjypsW"] = "/bin/sfx.lua"
+package.pkgType = "com.pastebin"
+package.unicornSpec = "v1.0.0"
+
+return package
