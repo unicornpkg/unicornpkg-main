70a88e8a1d122edbf1b8ba6aac7658025b54cf9c
Tomodachi94 <tomodachi94@protonmail.com>
2025-09-04 21:15:53 -0700

    Try something

--- unix-path-bootstrap.lua
+++ unix-path-bootstrap.lua
@@ -1,15 +1,15 @@
-local package = {}
-package.name = "unix-path-bootstrap"
-package.desc = "Adds /bin to the shell's path and /usr/share/help to the help path."
-package.instdat = {}
-package.instdat.repo_owner = "Tomodachi94"
-package.instdat.repo_name = "57b52cef2ad117c0b4130429dc80964a"
-package.instdat.repo_ref = "0dbe8a7ef58ac6f6a562410fafe7654301ffc678"
-package.instdat.filemaps = {}
-package.instdat.filemaps["unix-path-bootstrap.lua"] = "/bin/unix-path-bootstrap.lua"
-package.instdat.filemaps["unix-path-bootstrap-startup.lua"] = "/startup/unix-path-bootstrap.lua"
-package.dirs = { "/startup" }
-package.pkgType = "com.github.gist"
-package.unicornSpec = "v1.0.0"
+local p = {}
+p.name = "unix-path-bootstrap"
+p.desc = "Adds /bin to the shell's path and /usr/share/help to the help path."
+p.instdat = {}
+p.instdat.repo_owner = "Tomodachi94"
+p.instdat.repo_name = "57b52cef2ad117c0b4130429dc80964a"
+p.instdat.repo_ref = "0dbe8a7ef58ac6f6a562410fafe7654301ffc678"
+p.instdat.filemaps = {}
+p.instdat.filemaps["unix-path-bootstrap.lua"] = "/bin/unix-path-bootstrap.lua"
+p.instdat.filemaps["unix-path-bootstrap-startup.lua"] = "/startup/unix-path-bootstrap.lua"
+p.dirs = { "/startup" }
+p.pkgType = "com.github.gist"
+p.unicornSpec = "v1.0.0"
 
-return package
+return p
