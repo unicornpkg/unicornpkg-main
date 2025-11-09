e06205ee2d7e47ec19b6db1f854cbc14f1e61a99
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-08 23:15:26 -0800

    mcfly: init

---
+++ mcfly.lua
@@ -0,0 +1,13 @@
+local package = {}
+package.name = "mcfly"
+package.desc = "A very basic test framework for ComputerCraft"
+package.instdat = {}
+package.instdat.repo_owner = "cc-tweaked"
+package.instdat.repo_name = "CC-Tweaked"
+package.instdat.repo_ref = "mc-1.20.x"
+package.instdat.filemaps = {}
+package.instdat.filemaps["projects/core/src/test/resources/test-rom/mcfly.lua"] = "/bin/mcfly.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
