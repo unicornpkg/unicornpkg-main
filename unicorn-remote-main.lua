d4338f27bc0e6f761dd76cfaab9ec483dace511b
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-08 23:07:07 -0800

    unicorn-remote-main: init at 2025.11.8

---
+++ unicorn-remote-main.lua
@@ -0,0 +1,10 @@
+local package = {}
+package.name = "unicorn-remote-main"
+package.pkgType = "local.string"
+package.version = "2025.11.8"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["https://unicornpkg.github.io/unicornpkg-main"] = "/etc/unicorn/remotes/90_main.txt"
+package.unicornSpec = "v1.0.0"
+
+return package
