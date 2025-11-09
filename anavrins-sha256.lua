e1e6216264c67dbc94d14453827636f65e8bacf9
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-08 23:03:34 -0800

    anavrins-sha256: init

---
+++ anavrins-sha256.lua
@@ -0,0 +1,11 @@
+local package = {}
+package.name = "anavrins-sha256"
+package.desc = "SHA-256, HMAC and PBKDF2 functions in ComputerCraft"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["6UV4qfNF"] = "/lib/sha256.lua"
+package.instdat.filemaps["q2SQ7eRg"] = "/usr/share/help/sha256.txt"
+package.pkgType = "com.pastebin"
+package.unicornSpec = "v1.0.0"
+
+return package
