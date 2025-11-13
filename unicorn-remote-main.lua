06bb91fb16d6d927dcafd76173fbb847ef329a94
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-13 09:12:52 -0800

    unicorn-remote-main: compensate for "legacy" installs

--- unicorn-remote-main.lua
+++ unicorn-remote-main.lua
@@ -1,10 +1,17 @@
 local package = {}
 package.name = "unicorn-remote-main"
 package.pkgType = "local.string"
-package.version = "2025.11.8"
+package.version = "2025.11.13"
 package.instdat = {}
 package.instdat.filemaps = {}
 package.instdat.filemaps["https://unicornpkg.github.io/unicornpkg-main"] = "/etc/unicorn/remotes/90_main.txt"
+package.script = {}
+package.script.preinstall = [[
+    if fs.exists("/etc/unicorn/remotes/90_main.txt") then
+        print("package(unicorn-remote-main): found an existing file at /etc/unicorn/remotes/90_main.txt, deleting")
+        fs.delete("/etc/unicorn/remotes/90_main.txt")
+    end
+]]
 package.unicornSpec = "v1.0.0"
 
 return package
