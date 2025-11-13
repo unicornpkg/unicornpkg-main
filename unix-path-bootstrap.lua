94757157013b3e42536139f1f2fc718bcc97f576
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-13 13:10:16 -0800

    unix-path-bootstrap: fetch from new home at libunicornpkg repo

--- unix-path-bootstrap.lua
+++ unix-path-bootstrap.lua
@@ -1,15 +1,14 @@
-local p = {}
-p.name = "unix-path-bootstrap"
-p.desc = "Adds /bin to the shell's path and /usr/share/help to the help path."
-p.instdat = {}
-p.instdat.repo_owner = "Tomodachi94"
-p.instdat.repo_name = "57b52cef2ad117c0b4130429dc80964a"
-p.instdat.repo_ref = "0dbe8a7ef58ac6f6a562410fafe7654301ffc678"
-p.instdat.filemaps = {}
-p.instdat.filemaps["unix-path-bootstrap.lua"] = "/bin/unix-path-bootstrap.lua"
-p.instdat.filemaps["unix-path-bootstrap-startup.lua"] = "/startup/unix-path-bootstrap.lua"
-p.dirs = { "/startup" }
-p.pkgType = "com.github.gist"
-p.unicornSpec = "v1.0.0"
+local package = {}
+package.name = "unix-path-bootstrap"
+package.version = "1.3.1"
+package.desc = "Adds /bin to the shell's path and /usr/share/help to the help path."package.version = "1.1.0"
+package.instdat = {}
+package.instdat.repo_owner = "unicornpkg"
+package.instdat.repo_name = "libunicornpkg"
+package.instdat.repo_ref = "v" .. package.version
+package.instdat.filemaps = {}
+package.instdat.filemaps["extras/unix-path-bootstrap/unix-path-bootstrap.lua"] = "/startup/unix-path-bootstrap.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
 
-return p
+return package
