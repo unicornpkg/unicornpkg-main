f1a0438973df7012b80fa39477ce1573f3f9b870
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-13 12:16:29 -0800

    shrekbox: init

---
+++ shrekbox.lua
@@ -0,0 +1,13 @@
+local package = {}
+package.name = "shrekbox"
+package.desc = [[A library to make working with "pixel" and "bixel" graphics easier.]]
+package.instdat = {}
+package.instdat.repo_owner = "ShreksHellraiser"
+package.instdat.repo_name = "shrekbox"
+package.instdat.repo_ref = "main"
+package.instdat.filemaps = {}
+package.instdat.filemaps["shrekbox.lua"] = "/lib/shrekbox.lua"
+package.pkgType = "org.codeberg"
+package.unicornSpec = "v1.0.0"
+
+return package
