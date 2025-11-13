7a403fd5aac1e95cfdb3a9adc4562c5ed1677e2b
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-13 10:24:02 -0800

    unicorn-cli: install docs too

--- unicorn-cli.lua
+++ unicorn-cli.lua
@@ -8,7 +8,9 @@ package.instdat.repo_name = "libunicornpkg"
 package.instdat.repo_ref = "cli/v" .. package.version
 package.instdat.filemaps = {}
 package.instdat.filemaps["cli/hoof.lua"] = "/bin/hoof.lua"
+package.instdat.filemaps["docs/cli/hoof.md"] = "/usr/share/help/hoof.md"
 package.instdat.filemaps["cli/unicorntool.lua"] = "/bin/unicorntool.lua"
+package.instdat.filemaps["docs/cli/unicorntool.md"] = "/usr/share/help/unicorntool.md"
 package.pkgType = "com.github"
 package.unicornSpec = "v1.0.0"
 
