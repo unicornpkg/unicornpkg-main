42798538b18f9577cf7ece3e8c4e3ded457491cb
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-13 11:46:06 -0800

    pixelui2: init

---
+++ pixelui2.lua
@@ -0,0 +1,15 @@
+local package = {}
+package.name = "pixelui2"
+package.desc = "A modern UI framework for CC: Tweaked."
+package.instdat = {}
+package.instdat.repo_owner = "Shlomo1412"
+package.instdat.repo_name = "PixelUI-v2"
+package.instdat.repo_ref = "main"
+package.instdat.filemaps = {}
+package.instdat.filemaps["compressed/pixelui.lua"] = "/lib/pixelui.lua"
+rel = {}
+rel.conflicts = {"pixelui"}
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
