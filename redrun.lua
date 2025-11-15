da1ad2dd4da61399579662e7e51774ccaf40df9c
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 15:33:06 -0800

    redrun: init

---
+++ redrun.lua
@@ -0,0 +1,16 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://gist.github.com/MCJack123/473475f07b980d57dd2bd818026c97e8
+
+local package = {}
+package.name = "redrun"
+package.desc = "A very tiny background task runner for CC using the native top-level coroutine"
+package.instdat = {}
+package.instdat.repo_owner = "MCJack123"
+package.instdat.repo_name = "473475f07b980d57dd2bd818026c97e8"
+package.instdat.repo_ref = "7a5c442c4064ff0822032e49b7ee1964fa3783e6"
+package.instdat.filemaps = {}
+package.instdat.filemaps["redrun.lua"] = "/lib/redrun.lua"
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
+
+return package
