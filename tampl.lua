612b0cd85121b9a1db59b4ad785aaded996f848f
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 15:57:11 -0800

    tampl: init

---
+++ tampl.lua
@@ -0,0 +1,17 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://github.com/9551-Dev/tampl
+
+local package = {}
+package.name = "tampl"
+package.desc = "Lua code parsing, patching and injection library"
+package.licensing = "MIT"
+package.instdat = {}
+package.instdat.repo_owner = "9551-Dev"
+package.instdat.repo_name = "tampl"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["tampl.lua"] = "/lib/tampl.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
