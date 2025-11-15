fc694ad4cf50c5268fb1c9a34b25d2a7200a86dc
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 15:48:20 -0800

    dbprotect: init

---
+++ dbprotect.lua
@@ -0,0 +1,15 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://gist.github.com/MCJack123/4cf6fc941a2d412b4195caafb9636363
+
+local package = {}
+package.name = "dbprotect"
+package.desc = "Protect your functions from the debug library"
+package.licensing = "CC0"
+package.instdat = {}
+package.instdat.repo_owner = "MCJack123"
+package.instdat.repo_name = "4cf6fc941a2d412b4195caafb9636363"
+package.instdat.repo_ref = "10ceb98560f1b522ca2a1317cefdd8d4c8a5d5ca"
+package.instdat.filemaps = {}
+package.instdat.filemaps["dbprotect.lua"] = "/usr/libexec/dbprotect.lua"
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
