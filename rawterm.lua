f60d3dff89f8033cccd96aa5da5d9ddf49c3ce46
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 15:37:06 -0800

    rawterm: init

---
+++ rawterm.lua
@@ -0,0 +1,16 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://gist.github.com/MCJack123/50b211c55ceca4376e51d33435026006
+
+local package = {}
+package.name = "rawterm"
+package.desc = "CraftOS-PC raw mode client/server API for ComputerCraft"
+package.instdat = {}
+package.instdat.repo_owner = "MCJack123"
+package.instdat.repo_name = "50b211c55ceca4376e51d33435026006"
+package.instdat.repo_ref = "cefbd568f50fbc9599306f5a692e90aed7aa9517"
+package.instdat.filemaps = {}
+package.instdat.filemaps["rawterm.lua"] = "/lib/rawterm.lua"
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
+
+return package
