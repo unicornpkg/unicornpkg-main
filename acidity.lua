c4fedd7440fe77308488b2b2da6f42ecd0b1548c
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 19:18:56 -0800

    acidity: init

---
+++ acidity.lua
@@ -0,0 +1,13 @@
+local package = {}
+package.name = "acidity"
+package.desc = "Procedual world generation-oriented noise generation library"
+package.instdat = {}
+package.instdat.repo_owner = "9551-Dev"
+package.instdat.repo_name = "acidity"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["acidity.lua"] = "/lib/acidity.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
