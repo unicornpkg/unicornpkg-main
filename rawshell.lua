2d40029aeee2f5b5c6d625143ff23d0c8e4e7b71
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 16:07:22 -0800

    rawshell: init

---
+++ rawshell.lua
@@ -0,0 +1,18 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://gist.github.com/MCJack123/8c8861e5e3082d2bed18d07641b5b2cc
+
+local package = {}
+package.name = "rawshell"
+package.desc = "Remote shell program for ComputerCraft featuring passwords, encryption, and background processing"
+package.instdat = {}
+package.instdat.repo_owner = "MCJack123"
+package.instdat.repo_name = "8c8861e5e3082d2bed18d07641b5b2cc"
+package.instdat.repo_ref = "18a7aa541e4b31f0634245a81c7aa59352431b1b"
+package.instdat.filemaps = {}
+package.instdat.filemaps["rawshell.lua"] = "/bin/rawshell.lua"
+package.rel = {}
+package.rel.depends = {"rawterm"}
+package.pkgType = "com.github.gist"
+package.unicornSpec = "v1.0.0"
+
+return package
