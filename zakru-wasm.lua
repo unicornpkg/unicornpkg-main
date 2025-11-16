44f45f639f2ea75d42684d71e81ed6bcbca40438
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 22:58:15 -0800

    zakru-wasm: init

---
+++ zakru-wasm.lua
@@ -0,0 +1,17 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://github.com/Zakru/lua-wasm
+
+local package = {}
+package.name = "zakru-wasm"
+package.desc = "Pure Lua implementation of the WebAssembly runtime"
+package.licensing = "MIT"
+package.instdat = {}
+package.instdat.repo_owner = "Zakru"
+package.instdat.repo_name = "lua-wasm"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["luawasm.lua"] = "/lib/luawasm.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
