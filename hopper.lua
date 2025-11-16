3c1a632bdf5f7c861583fefea33fc522409674fc
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 18:58:59 -0800

    hopper: init at 1.4.5

---
+++ hopper.lua
@@ -0,0 +1,14 @@
+local package = {}
+package.name = "hopper"
+package.desc = "The FFmpeg of Minecraft item transportation: move items and fluids from A to B without any hassle"
+package.version = "1.4.5"
+package.instdat = {}
+package.instdat.repo_owner = "umnikos"
+package.instdat.repo_name = "hopper.lua"
+package.instdat.repo_ref = "v" .. package.version
+package.instdat.filemaps = {}
+package.instdat.filemaps["hopper.lua"] = "/bin/hopper.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
