7a49af507fd14f849890d3a719bb4f2c8674ae18
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-15 19:48:27 -0800

    ecnet2: init at 2.0.0

---
+++ ecnet2.lua
@@ -0,0 +1,28 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://github.com/migeyel/ecnet
+
+local package = {}
+package.name = "ecnet2"
+package.desc = "Mid-level encrypted networking library for CC:Tweaked."
+package.licensing = "MIT"
+package.version = "2.0.0"
+package.instdat = {}
+package.instdat.repo_owner = "migeyel"
+package.instdat.repo_name = "ecnet"
+package.instdat.repo_ref = "v" .. package.version
+package.instdat.filemaps["ecnet2/CipherState.lua"] = "/lib/ecnet2/CipherState.lua"
+package.instdat.filemaps["ecnet2/Connection.lua"] = "/lib/ecnet2/Connection.lua"
+package.instdat.filemaps["ecnet2/HandshakeState.lua"] = "/lib/ecnet2/HandshakeState.lua"
+package.instdat.filemaps["ecnet2/Identity.lua"] = "/lib/ecnet2/Identity.lua"
+package.instdat.filemaps["ecnet2/Listener.lua"] = "/lib/ecnet2/Listener.lua"
+package.instdat.filemaps["ecnet2/Protocol.lua"] = "/lib/ecnet2/Protocol.lua"
+package.instdat.filemaps["ecnet2/SymmetricState.lua"] = "/lib/ecnet2/SymmetricState.lua"
+package.instdat.filemaps["ecnet2/addressEncoder.lua"] = "/lib/ecnet2/addressEncoder.lua"
+package.instdat.filemaps["ecnet2/class.lua"] = "/lib/ecnet2/class.lua"
+package.instdat.filemaps["ecnet2/constants.lua"] = "/lib/ecnet2/constants.lua"
+package.instdat.filemaps["ecnet2/ecnetd.lua"] = "/lib/ecnet2/ecnetd.lua"
+package.instdat.filemaps["ecnet2/init.lua"] = "/lib/ecnet2/init.lua"
+package.instdat.filemaps["ecnet2/modems.lua"] = "/lib/ecnet2/modems.lua"
+package.instdat.filemaps["ecnet2/uid.lua"] = "/lib/ecnet2/uid.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
