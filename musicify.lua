31e967791470465599f6271a067469ed872b44b6
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-16 18:37:17 -0800

    musicify: 2.5.0 - 3.2.1

--- musicify.lua
+++ musicify.lua
@@ -3,20 +3,20 @@
 local package = {}
 package.name = "musicify"
 package.desc = "Client for song repositories"
-package.version = "2.5.0"
+package.version = "3.2.1"
 package.instdat = {}
 package.instdat.repo_owner = "knijn"
 package.instdat.repo_name = "musicify"
-package.instdat.repo_ref = "2.5.0"
+package.instdat.repo_ref = package.version
 package.instdat.filemaps = {}
 package.instdat.filemaps["musicify.lua"] = "/bin/musicify.lua"
 package.rel = {}
 package.rel.depends = {
 	"semver",
 	"youcubeapi",
-	"basalt",
+	"basalt"
 }
-package.pkgType = "com.github"
+package.pkgType = "com.github.release"
 package.unicornSpec = "v1.0.0"
 
 return package
