b5591d55bfa3beba0e880e3fdf67660d875cd170
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-16 18:43:41 -0800

    argparse: 0.7.2 -> 0.7.3

--- argparse.lua
+++ argparse.lua
@@ -1,11 +1,11 @@
 local package = {}
 package.name = "argparse"
 package.desc = "Feature-rich command line parser for ComputerCraft"
-package.version = "0.7.2"
+package.version = "0.7.3"
 package.instdat = {}
 package.instdat.repo_owner = "Commandcracker"
 package.instdat.repo_name = "cc-argparse"
-package.instdat.repo_ref = "v0.7.2"
+package.instdat.repo_ref = "v" .. package.version
 package.instdat.filemaps = {}
 package.instdat.filemaps["argparse.min.lua"] = "/lib/argparse.lua"
 package.pkgType = "com.github.release"
