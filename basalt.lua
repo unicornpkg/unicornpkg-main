23d026b56471bf6ed25792d670e6dcb74e448d39
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-16 18:48:23 -0800

    basalt: 1.6.4 -> 1.7.0

--- basalt.lua
+++ basalt.lua
@@ -3,11 +3,11 @@
 local package = {}
 package.name = "basalt"
 package.desc = "UI Framework for CC:Tweaked"
-package.version = "1.6.4"
+package.version = "1.7.0"
 package.instdat = {}
 package.instdat.repo_owner = "Pyroxenium"
 package.instdat.repo_name = "Basalt"
-package.instdat.repo_ref = "v1.6.4"
+package.instdat.repo_ref = "v" package.version
 package.instdat.filemaps = {}
 package.instdat.filemaps["basalt.lua"] = "/lib/basalt.lua"
 package.pkgType = "com.github.release"
