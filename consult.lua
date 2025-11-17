2bf33408171b1ac12ba393dda91ea885b3111c92
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-16 18:45:51 -0800

    consult: 1.4.3 -> 1.4.4; fix name

--- consult.lua
+++ consult.lua
@@ -1,11 +1,11 @@
 local package = {}
-package.name = "cosu"
+package.name = "consult"
 package.desc = "Modular text editor for ComputerCraft"
-package.version = "1.4.3"
+package.version = "1.4.4"
 package.instdat = {}
 package.instdat.repo_owner = "1Turtle"
 package.instdat.repo_name = "CONSULT"
-package.instdat.repo_ref = "1.4.3"
+package.instdat.repo_ref = package.version
 package.instdat.filemaps = {}
 package.instdat.filemaps["cosu.lua"] = "/bin/cosu.lua"
 package.pkgType = "com.github"
