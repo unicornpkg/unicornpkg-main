50a1e9c88a495734438a7f2bb4ad1952cfa419c4
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-19 09:26:00 -0800

    rxi-json: init at 0.1.2

---
+++ rxi-json.lua
@@ -0,0 +1,14 @@
+local package = {}
+package.name = "rxi-json"
+package.desc = "A lightweight JSON library for Lua"
+package.version = "0.1.2"
+package.instdat = {}
+package.instdat.repo_owner = "rxi"
+package.instdat.repo_name = "json.lua"
+package.instdat.repo_ref = "v" .. package.version
+package.instdat.filemaps = {}
+package.instdat.filemaps["json.lua"] = "/lib/json.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
