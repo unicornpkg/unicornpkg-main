822d7322630b7b7fbecb61f470ab8c34df5b0bff
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-19 19:15:32 -0800

    luacov: init at 0.16.0

---
+++ luacov.lua
@@ -0,0 +1,33 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://github.com/lunarmodules/luacov
+
+local package = {}
+package.name = "luacov"
+package.desc = "LuaCov is a simple coverage analyzer for Lua code."
+package.version = "0.16.0"
+package.licensing = "MIT"
+package.instdat = {}
+package.instdat.repo_owner = "lunarmodules"
+package.instdat.repo_name = "luacov"
+package.instdat.repo_ref = "v" .. package.version
+package.instdat.filemaps = {}
+package.instdat.filemaps["/lib/luacov.lua"] = "/lib/luacov.lua"
+package.instdat.filemaps["/lib/luacov/defaults.lua"] = "/lib/luacov/defaults.lua"
+package.instdat.filemaps["/lib/luacov/hook.lua"] = "/lib/luacov/hook.lua"
+package.instdat.filemaps["/lib/luacov/linescanner.lua"] = "/lib/luacov/linescanner.lua"
+package.instdat.filemaps["/lib/luacov/reporter.lua"] = "/lib/luacov/reporter.lua"
+package.instdat.filemaps["/lib/luacov/reporter/default.lua"] = "/lib/luacov/reporter/default.lua"
+package.instdat.filemaps["/lib/luacov/reporter/html.lua"] = "/lib/luacov/reporter/html.lua"
+package.instdat.filemaps["/lib/luacov/reporter/html/static/lang-lua.js"] = "/lib/luacov/reporter/html/static/lang-lua.js"
+package.instdat.filemaps["/lib/luacov/reporter/html/static/prettify.js"] = "/lib/luacov/reporter/html/static/prettify.js"
+package.instdat.filemaps["/lib/luacov/reporter/html/static/report.css"] = "/lib/luacov/reporter/html/static/report.css"
+package.instdat.filemaps["/lib/luacov/reporter/html/static/report.js"] = "/lib/luacov/reporter/html/static/report.js"
+package.instdat.filemaps["/lib/luacov/reporter/html/template.lua"] = "/lib/luacov/reporter/html/template.lua"
+package.instdat.filemaps["/lib/luacov/runner.lua"] = "/lib/luacov/runner.lua"
+package.instdat.filemaps["/lib/luacov/stats.lua"] = "/lib/luacov/stats.lua"
+package.instdat.filemaps["/lib/luacov/tick.lua"] = "/lib/luacov/tick.lua"
+package.instdat.filemaps["/lib/luacov/util.lua"] = "/lib/luacov/util.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
