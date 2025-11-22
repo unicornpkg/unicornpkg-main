bcd6242f3abf7555a9fbb51090bf6e2e881d89f9
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-21 23:09:01 -0800

    turtlegistics: init

---
+++ turtlegistics.lua
@@ -0,0 +1,16 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://github.com/dmarcuse/turtlegistics
+
+local package = {}
+package.name = "turtlegistics"
+package.desc = "Item management using CC and Plethora"
+package.instdat = {}
+package.instdat.repo_owner = "dmarcuse"
+package.instdat.repo_name = "turtlegistics"
+package.instdat.repo_ref = "master"
+package.instdat.filemaps = {}
+package.instdat.filemaps["turtlegistics.lua"] = "/bin/turtlegistics.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
