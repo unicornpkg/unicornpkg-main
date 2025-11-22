a953ef853fac30e8341c8bdff00a124cc5dea272
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-22 14:13:56 -0800

    libunicornpkg: init at 1.4.9

---
+++ libunicornpkg.lua
@@ -0,0 +1,36 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://github.com/unicornpkg/libunicornpkg
+
+local package = {}
+package.name = "libunicornpkg"
+package.version = "1.4.0"
+package.desc = "Library implementation of unicornpkg, a package manager for ComputerCraft."
+package.licensing = "Apache-2.0"
+package.instdat = {}
+package.instdat.repo_owner = "unicornpkg"
+package.instdat.repo_name = "libunicornpkg"
+package.instdat.repo_ref = "v" .. package.version
+package.instdat.filemaps = {}
+package.instdat.filemaps["unicorn/constants.lua"] = "unicorn/constants.lua"
+package.instdat.filemaps["unicorn/core.lua"] = "unicorn/core.lua"
+package.instdat.filemaps["unicorn/init.lua"] = "unicorn/init.lua"
+package.instdat.filemaps["unicorn/provider/com/github/gist.lua"] = "unicorn/provider/com/github/gist.lua"
+package.instdat.filemaps["unicorn/provider/com/github/init.lua"] = "unicorn/provider/com/github/init.lua"
+package.instdat.filemaps["unicorn/provider/com/github/release.lua"] = "unicorn/provider/com/github/release.lua"
+package.instdat.filemaps["unicorn/provider/com/gitlab.lua"] = "unicorn/provider/com/gitlab.lua"
+package.instdat.filemaps["unicorn/provider/com/pastebin.lua"] = "unicorn/provider/com/pastebin.lua"
+package.instdat.filemaps["unicorn/provider/ht/sr.lua"] = "unicorn/provider/ht/sr.lua"
+package.instdat.filemaps["unicorn/provider/io/sc3/p.lua"] = "unicorn/provider/io/sc3/p.lua"
+package.instdat.filemaps["unicorn/provider/local/generic.lua"] = "unicorn/provider/local/generic.lua"
+package.instdat.filemaps["unicorn/provider/local/nothing.lua"] = "unicorn/provider/local/nothing.lua"
+package.instdat.filemaps["unicorn/provider/local/string.lua"] = "unicorn/provider/local/string.lua"
+package.instdat.filemaps["unicorn/provider/net/launchpad/git.lua"] = "unicorn/provider/net/launchpad/git.lua"
+package.instdat.filemaps["unicorn/provider/org/archive.lua"] = "unicorn/provider/org/archive.lua"
+package.instdat.filemaps["unicorn/provider/org/bitbucket.lua"] = "unicorn/provider/org/bitbucket.lua"
+package.instdat.filemaps["unicorn/provider/org/codeberg.lua"] = "unicorn/provider/org/codeberg.lua"
+package.instdat.filemaps["unicorn/provider/org/softwareheritage/FetchContentFromSWHID.graphql"] = "unicorn/provider/org/softwareheritage/FetchContentFromSWHID.graphql"
+package.instdat.filemaps["unicorn/provider/org/softwareheritage/archive.lua"] = "unicorn/provider/org/softwareheritage/archive.lua"
+package.instdat.filemaps["unicorn/remote.lua"] = "unicorn/remote.lua"
+package.instdat.filemaps["unicorn/util.lua"] = "unicorn/util.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
