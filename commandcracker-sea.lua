373c0c939a1e600d88e21fda197b0cc625535ca1
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-24 11:10:03 -0800

    commandcracker-sea: init

---
+++ commandcracker-sea.lua
@@ -0,0 +1,17 @@
+-- Generated with https://github.com/unicornpkg/easyunicornpkg
+-- https://github.com/commandcracker/cc-sea
+
+local package = {}
+package.name = "commandcracker-sea"
+package.desc = "self-extracting archive for ComputerCraft"
+package.licensing = "Unlicense"
+package.instdat = {}
+package.instdat.repo_owner = "Commandcracker"
+package.instdat.repo_name = "cc-sea"
+package.instdat.repo_ref = "main"
+package.instdat.filemaps = {}
+package.instdat.filemaps["/bin/ccsfx.lua"] = "ccsfx.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+
+return package
