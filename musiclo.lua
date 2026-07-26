ea8a9ecdcd6a4845d89754ce7ce0616f92101543
Tomodachi94 <tomodachi94@protonmail.com>
2026-07-26 07:30:03 +0000

    Create musiclo.lua

---
+++ musiclo.lua
@@ -0,0 +1,15 @@
+-- https://gitlab.com/monosoft2/musiclo
+
+local package = {}
+package.name = "musiclo"
+package.desc = "A CC:Tweaked music player that's easy to use and looks sleek, phew!"
+package.instdat = {}
+package.instdat.repo_owner = "monosoft2"
+package.instdat.repo_name = "musiclo"
+package.instdat.repo_ref = "main"
+package.instdat.filemaps = {}
+package.instdat.filemaps["music.lua"] = "/bin/music.lua"
+package.pkgType = "com.gitlab"
+package.unicornSpec = "v1.0.0"
+
+return package
